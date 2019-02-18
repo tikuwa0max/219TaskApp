5cf8df641bdd2a6038d925585a45cd5f393e8823
tikuwa0max <itizawa@ruri.waseda.jp>
2019-02-18 21:10:02 +0900

    検索前まで

--- app/src/main/java/ichizawa/mikitaka/techacademy/taskapp/MainActivity.kt
+++ app/src/main/java/ichizawa/mikitaka/techacademy/taskapp/MainActivity.kt
@@ -1,14 +1,25 @@
 package ichizawa.mikitaka.techacademy.taskapp
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.widget.ListView
+import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
+import io.realm.RealmChangeListener
+import io.realm.Sort
+import android.content.Intent
+import android.support.v7.app.AlertDialog
 
+const val EXTRA_TASK = "ichizawa.mikitaka.techacademy.taskapp.TASK"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mRealm: Realm
+    private val mRealmListener = object : RealmChangeListener<Realm> {
+        override fun onChange(element: Realm) {
+            reloadListView()
+        }
+    }
 
     private lateinit var mTaskAdapter: TaskAdapter
 
@@ -16,34 +27,91 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fab.setOnClickListener { view ->
+            val intent = Intent(this@MainActivity, InputActivity::class.java)
+            startActivity(intent)
+        }
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            
         }
 
+        // Realmの設定
+        mRealm = Realm.getDefaultInstance()
+        mRealm.addChangeListener(mRealmListener)
+
+        // ListViewの設定
         mTaskAdapter = TaskAdapter(this@MainActivity)
 
-//      ListViewのタップ
-        listView1.setOnItemClickListener{parent, view, position, id ->
-
+        // ListViewをタップしたときの処理
+        listView1.setOnItemClickListener { parent, _, position, _ ->
+            // 入力・編集する画面に遷移させる
+            val task = parent.adapter.getItem(position) as Task
+            val intent = Intent(this@MainActivity, InputActivity::class.java)
+            intent.putExtra(EXTRA_TASK, task.id)
+            startActivity(intent)
         }
+
         // ListViewを長押ししたときの処理
-        listView1.setOnItemLongClickListener { parent, view, position, id ->
+        listView1.setOnItemLongClickListener { parent, _, position, _ ->
             // タスクを削除する
+            val task = parent.adapter.getItem(position) as Task
+
+            // ダイアログを表示する
+            val builder = AlertDialog.Builder(this@MainActivity)
+
+            builder.setTitle("削除")
+            builder.setMessage(task.title + "を削除しますか")
+
+            builder.setPositiveButton("OK"){_, _ ->
+                val results = mRealm.where(Task::class.java).equalTo("id", task.id).findAll()
+
+                mRealm.beginTransaction()
+                results.deleteAllFromRealm()
+                mRealm.commitTransaction()
+
+                val resultIntent = Intent(applicationContext, TaskAlarmReceiver::class.java)
+                val resultPendingIntent = PendingIntent.getBroadcast(
+                    this@MainActivity,
+                    task.id,
+                    resultIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+
+                val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+                alarmManager.cancel(resultPendingIntent)
+                
+                reloadListView()
+            }
+
+            builder.setNegativeButton("CANCEL", null)
+
+            val dialog = builder.create()
+            dialog.show()
+
             true
         }
 
-        reloadListview()
+        reloadListView()
     }
 
-    private fun reloadListview(){
+    private fun reloadListView() {
+        // Realmデータベースから、「全てのデータを取得して新しい日時順に並べた結果」を取得
+        val taskRealmResults = mRealm.where(Task::class.java).findAll().sort("date", Sort.DESCENDING)
 
-        val taskList = mutableListOf("aaa","bbb","ccc")
+        // 上記の結果を、TaskList としてセットする
+        mTaskAdapter.taskList = mRealm.copyFromRealm(taskRealmResults)
 
-        mTaskAdapter.taskList = taskList
+        // TaskのListView用のアダプタに渡す
         listView1.adapter = mTaskAdapter
+
+        // 表示を更新するために、アダプターにデータが変更されたことを知らせる
         mTaskAdapter.notifyDataSetChanged()
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+
+        mRealm.close()
+    }
 }
--- app/src/main/java/ichizawa/mikitaka/techacademy/taskapp/TaskAdapter.kt
+++ app/src/main/java/ichizawa/mikitaka/techacademy/taskapp/TaskAdapter.kt
@@ -1,15 +1,17 @@
 package ichizawa.mikitaka.techacademy.taskapp
-
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TaskAdapter(context: Context): BaseAdapter() {
+
     private val mLayoutInflater: LayoutInflater
-    var taskList = mutableListOf<String>()
+    var taskList = mutableListOf<Task>()
 
     init {
         this.mLayoutInflater = LayoutInflater.from(context)
@@ -24,23 +26,21 @@ class TaskAdapter(context: Context): BaseAdapter() {
     }
 
     override fun getItemId(position: Int): Long {
-        return 0
+        return taskList[position].id.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view:View = convertView ?: mLayoutInflater.inflate(android.R.layout.simple_expandable_list_item_2, null)
+        val view: View = convertView ?: mLayoutInflater.inflate(android.R.layout.simple_list_item_2, null)
 
         val textView1 = view.findViewById<TextView>(android.R.id.text1)
         val textView2 = view.findViewById<TextView>(android.R.id.text2)
 
-        textView1.text = taskList[position]
+        textView1.text = taskList[position].title
+
+        val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.JAPANESE)
+        val date = taskList[position].date
+        textView2.text = simpleDateFormat.format(date)
 
         return view
     }
-
-
-
-
-
-
 }
